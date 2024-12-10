4497399ee90e09f9eda8d51263605a9632280efc
checkinindza <tomasfilipavicius13@gmail.com>
2024-12-10 03:07:24 +0200

    Initial commit

---
+++ src/main/java/kontrolins/lab2weblayer/Lab2WebLayerApplication.java
@@ -0,0 +1,13 @@
+package kontrolins.lab2weblayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2WebLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2WebLayerApplication.class, args);
+    }
+
+}
